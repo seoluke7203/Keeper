0b6367cf696c710003c10d6faa34d1b556325009
seoluke7203 <seoluke7203@hotmail.com>
2023-06-19 22:44:09 -0700

    Keeper Note

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HighlightIcom from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1><HighlightIcom></HighlightIcom>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
